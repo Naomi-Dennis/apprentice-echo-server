5faf32791831ea90c6234f8d3737d761155bb170
Naomi Dennis <naomi@8thlight.com>
2019-11-05 16:19:13 -0500

    Remove `Presenter` and create `Logger`

---
+++ src/main/java/echoserver/Logger.java
@@ -0,0 +1,15 @@
+package echoserver;
+
+public class Logger {
+    public Logger(Console console){
+        this.console = console;
+    }
+
+    public void log(String message){
+        console.outputToScreen(message);
+    }
+
+    private
+
+    Console console;
+}
