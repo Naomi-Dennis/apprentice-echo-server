fae9a0d7f849541f9da5f5f116e524fec1476cdd
Naomi Dennis <naomi@8thlight.com>
2019-11-05 11:38:07 -0500

    Merged EchoServerProgram with EchoServerSetup class * Removed `EchoServerSetup` class

--- src/main/java/EchoServerProgram.java
+++ src/main/java/EchoServerProgram.java
@@ -1,14 +1,22 @@
-import echoserver.CommandLinePortValidator;
-import echoserver.EchoServerSetup;
+import echoserver.*;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 
 public class EchoServerProgram {
     public static void main(String[] args) throws IOException {
-        EchoServerSetup basicEchoServerSetup = new EchoServerSetup();
+
         CommandLinePortValidator validator = new CommandLinePortValidator();
         final Integer SPECIFIED_PORT = validator.parsePort(args);
 
-        basicEchoServerSetup.run(SPECIFIED_PORT);
+        ServerSocket hostSocket = new ServerSocket(SPECIFIED_PORT);
+        HostServer hostServer = new HostServer(hostSocket);
+        Presenter hostScreen = new Presenter(new Console(System.out));
+        EchoServer echoServer = new EchoServer(hostServer, hostScreen);
+
+       EchoServer basicEchoServer = new EchoServer(hostServer, hostScreen);
+
+       hostScreen.showAwaitingInputMessageOnPort(SPECIFIED_PORT);
+       basicEchoServer.run();
     }
 }
