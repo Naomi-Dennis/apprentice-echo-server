9694f10887f857d4a6743a61020ae4d42875d769
Naomi Dennis <naomi@8thlight.com>
2019-12-05 12:59:12 -0500

    Fix "running jars in background" bug

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 eval "cd /opt"
-eval "(java -jar EchoService.jar &) && java -jar SimpleApplication.jar &"
+eval "(java -jar EchoService.jar > echo_log.txt &) && java -jar SimpleApplication.jar > simple_app_log.txt &"
