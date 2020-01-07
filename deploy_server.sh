9522bd5bee992d31e126f76b2a3f12919b64eaa1
Naomi Dennis <naomi@8thlight.com>
2020-01-07 14:43:30 -0500

    Fix AWS deploy error
    I noticed that all deployments would sometimes fail when a change
    was first made. To fix it, it would need to be redeloyed via CodeDeploy.
    This happened becuase sometimes the server would hang for too long,
    awaiting input, but it would still deploy and run the code.
    
    This fix ensures the bug doesn't happen and all input and output is
    run in a background process versus stdin/stdout.

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 eval "cd /opt"
-eval "(java -jar EchoService.jar > echo_log.txt &) && java -jar SimpleApplication.jar > simple_app_log.txt &"
+eval "(nohup java -jar EchoService.jar &) && (nohup java -jar SimpleApplication.jar &)"
